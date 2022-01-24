e0d9836ce9d09a0dbc250561a357a505b06fc9ab
365kim <365listener@gmail.com>
2022-01-24 23:49:18 +0900

    refactor: 01-4 result, aPerformance 변수명 변경하기

--- chapter01/01-4/amountFor.js
+++ chapter01/01-4/amountFor.js
@@ -1,25 +1,25 @@
-export function amountFor(perf, play) {
-  let thisAmount = 0;
+export function amountFor(aPerformance, play) {
+  let result = 0;
 
   switch (play.type) {
     case 'tragedy':
-      thisAmount = 40_000;
+      result = 40_000;
 
-      if (perf.audience > 30) {
-        thisAmount += 1_000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1_000 * (aPerformance.audience - 30);
       }
       break;
     case 'comedy':
-      thisAmount = 30_000;
+      result = 30_000;
 
-      if (perf.audience > 20) {
-        thisAmount += 10_000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10_000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
 
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
   }
-  return thisAmount;
+  return result;
 }
