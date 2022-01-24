3dee99c248e74c6254e4c67e3cc4ff1df81cb123
365kim <365listener@gmail.com>
2022-01-25 00:10:04 +0900

    refactor: 01-4 thisAmount 변수 인라인하기

--- chapter01/01-4/statement.js
+++ chapter01/01-4/statement.js
@@ -36,8 +36,6 @@ export function statement(invoice, plays) {
   }
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
-
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -47,8 +45,8 @@ export function statement(invoice, plays) {
     }
 
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(thisAmount / 100)} ${perf.audience}석\n`;
-    totalAmount += thisAmount;
+    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} ${perf.audience}석\n`;
+    totalAmount += amountFor(perf);
   }
   result += `총액 ${format(totalAmount / 100)}\n`;
   result += `적립 포인트 ${volumeCredits}점\n`;
