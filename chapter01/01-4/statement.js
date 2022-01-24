5c95be3cf47aa74d7f547bb9b32bf1cea0cc7a66
365kim <365listener@gmail.com>
2022-01-25 00:05:07 +0900

    refactor: 01-4 play 변수 인라인하기

--- chapter01/01-4/statement.js
+++ chapter01/01-4/statement.js
@@ -36,19 +36,18 @@ export function statement(invoice, plays) {
   }
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === play.type) {
+    if ('comedy' === playFor(perf).type) {
       volumeCredits += Math.floor(perf.audience / 5);
     }
 
     // 청구 내역을 출력한다.
-    result += `${play.name}: ${format(thisAmount / 100)} ${perf.audience}석\n`;
+    result += `${playFor(perf).name}: ${format(thisAmount / 100)} ${perf.audience}석\n`;
     totalAmount += thisAmount;
   }
   result += `총액 ${format(totalAmount / 100)}\n`;
