325f2e1ec9749275718dbdc966886b619f5a04f6
365kim <365listener@gmail.com>
2022-01-27 00:32:06 +0900

    refactor: 01-4 result, aPerformance 변수명 변경하기

--- chapter01/01-4/statement.js
+++ chapter01/01-4/statement.js
@@ -35,16 +35,16 @@ export function statement(invoice, plays) {
     return result;
   }
 
-  function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
 
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    result += Math.max(aPerformance.audience - 30, 0);
 
-    if ('comedy' === playFor(perf).type) {
-      volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(aPerformance).type) {
+      result += Math.floor(aPerformance.audience / 5);
     }
 
-    return volumeCredits;
+    return result;
   }
 
   for (let perf of invoice.performances) {
