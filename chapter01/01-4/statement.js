0fd40e1b70be5cf06f3144c2caba1e03136fe84c
365kim <365listener@gmail.com>
2022-01-27 00:54:29 +0900

    refactor: 01-4 totalAmount 반복문 쪼개기

--- chapter01/01-4/statement.js
+++ chapter01/01-4/statement.js
@@ -62,6 +62,8 @@ export function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} ${perf.audience}석\n`;
+  }
+  for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
 
