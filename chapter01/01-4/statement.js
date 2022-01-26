3ae4a8959393339833ca604d1d2e4f52fd84941f
365kim <365listener@gmail.com>
2022-01-27 00:49:24 +0900

    refactor: 01-4 totalVolumeCredits 임시변수를 쿼리함수로 바꾸기

--- chapter01/01-4/statement.js
+++ chapter01/01-4/statement.js
@@ -50,15 +50,21 @@ export function statement(invoice, plays) {
     return result;
   }
 
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
+
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} ${perf.audience}석\n`;
     totalAmount += amountFor(perf);
   }
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+
+  let volumeCredits = totalVolumeCredits();
 
   result += `총액 ${usd(totalAmount / 100)}\n`;
   result += `적립 포인트 ${volumeCredits}점\n`;
