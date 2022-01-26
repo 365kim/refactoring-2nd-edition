52a40a1dc0c2aa57ddebd794ff5b6607599d79c4
365kim <365listener@gmail.com>
2022-01-27 00:46:27 +0900

    refactor: 01-4 performances 반복문 쪼개기

--- chapter01/01-4/statement.js
+++ chapter01/01-4/statement.js
@@ -52,12 +52,14 @@ export function statement(invoice, plays) {
   }
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} ${perf.audience}석\n`;
     totalAmount += amountFor(perf);
   }
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
   result += `총액 ${usd(totalAmount / 100)}\n`;
   result += `적립 포인트 ${volumeCredits}점\n`;
 
