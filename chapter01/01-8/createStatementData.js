b8117b9ce13d538a5e95deeec2f986333fabba5b
365kim <365listener@gmail.com>
2022-02-05 17:01:17 +0900

    refactor: 01-8 amount 조건부 로직을 다형성으로 바꾸기

--- chapter01/01-8/createStatementData.js
+++ chapter01/01-8/createStatementData.js
@@ -34,7 +34,15 @@ export function createStatementData(invoice, plays) {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+
+    default:
+      throw new Error(`알 수 없는 장리: ${aPlay.type}`);
+  }
 }
 
 class PerformanceCalculator {
@@ -44,29 +52,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-
-    switch (this.play.type) {
-      case 'tragedy':
-        result = 40_000;
-
-        if (this.performance.audience > 30) {
-          result += 1_000 * (this.performance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30_000;
-
-        if (this.performance.audience > 20) {
-          result += 10_000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    }
-    return result;
+    throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
   }
 
   get volumeCredits() {
@@ -77,6 +63,32 @@ class PerformanceCalculator {
     if ('comedy' === this.play.type) {
       result += Math.floor(this.performance.audience / 5);
     }
+
+    return result;
+  }
+}
+
+/* 서브클래스 */
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40_000;
+
+    if (this.performance.audience > 30) {
+      result += 1_000 * (this.performance.audience - 30);
+    }
+
+    return result;
+  }
+}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30_000;
+
+    if (this.performance.audience > 20) {
+      result += 10_000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
 
     return result;
   }
