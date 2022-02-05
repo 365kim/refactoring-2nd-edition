80e16ea9ac39b744cca87b26229d61633934109d
365kim <365listener@gmail.com>
2022-02-05 16:47:28 +0900

    refactor: 01-8 amountFor 함수옮기기

--- chapter01/01-8/createStatementData.js
+++ chapter01/01-8/createStatementData.js
@@ -25,29 +25,7 @@ export function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40_000;
-
-        if (aPerformance.audience > 30) {
-          result += 1_000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30_000;
-
-        if (aPerformance.audience > 20) {
-          result += 10_000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 
   function volumeCreditsFor(aPerformance) {
@@ -75,5 +53,31 @@ class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
     this.play = aPlay;
+  }
+
+  get amount() {
+    let result = 0;
+
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40_000;
+
+        if (this.performance.audience > 30) {
+          result += 1_000 * (this.performance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30_000;
+
+        if (this.performance.audience > 20) {
+          result += 10_000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
   }
 }
