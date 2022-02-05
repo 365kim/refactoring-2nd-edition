5aee18709e00fbd857753d51aee59fa97cecec70
365kim <365listener@gmail.com>
2022-02-05 16:02:26 +0900

    refactor: 01-6 amountFor 함수옮기기

--- chapter01/01-4/statement.js
+++ chapter01/01-4/statement.js
@@ -8,31 +8,15 @@ export function statement(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
+
     result.play = playFor(result);
+    result.amount = amountFor(result);
+
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-}
-
-function renderPlainText(data, plays) {
-  let result = `청구내역 (고객명: ${data.customer})\n`;
-
-  for (let perf of data.performances) {
-    result += `${perf.play.name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
-  }
-
-  result += `총액 ${usd(totalAmount())}\n`;
-  result += `적립 포인트 ${totalVolumeCredits()}점\n`;
-
-  return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format(
-      aNumber / 100
-    );
   }
 
   function amountFor(aPerformance) {
@@ -60,6 +44,25 @@ function renderPlainText(data, plays) {
     }
     return result;
   }
+}
+
+function renderPlainText(data, plays) {
+  let result = `청구내역 (고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    result += `${perf.play.name}: ${usd(perf.amount)} ${perf.audience}석\n`;
+  }
+
+  result += `총액 ${usd(totalAmount())}\n`;
+  result += `적립 포인트 ${totalVolumeCredits()}점\n`;
+
+  return result;
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format(
+      aNumber / 100
+    );
+  }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
@@ -86,7 +89,7 @@ function renderPlainText(data, plays) {
     let result = 0;
 
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
