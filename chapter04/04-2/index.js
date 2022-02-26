5933a0b7ee7f6f69c253d47b7e74808ca935010e
365kim <365listener@gmail.com>
2022-02-26 21:57:10 +0900

    feat: 04-2 테스트할 샘플 코드 추가

---
+++ chapter04/04-2/index.js
@@ -0,0 +1,104 @@
+class Province {
+  constructor(doc) {
+    this._name = doc.name;
+    this._producers = [];
+    this._totalProduction = 0;
+    this._demand = doc._demand;
+    this._price = doc.price;
+    doc.producers.forEach((d) => this.addProducer(new this.producers(this, d)));
+  }
+
+  addProducer(arg) {
+    this._producer.push(arg);
+    this._totalProduction += arg.production;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get producers() {
+    return this._producers.slice();
+  }
+  get totalProduction() {
+    return this._totalProduction;
+  }
+  set totalProduction(arg) {
+    this._totalProduction = arg;
+  }
+  get demand() {
+    return this._demand;
+  }
+  set demand(arg) {
+    this._demand = parseInt(arg);
+  }
+  get price() {
+    return this._price;
+  }
+  set price(arg) {
+    this._price = parseInt(arg);
+  }
+  get shortfall() {
+    return this._demand - this.totalProduction;
+  }
+  get profit() {
+    return this.demandValue - this.demandCost;
+  }
+  get demandValue() {
+    return this.satisfiedDemand * this.price;
+  }
+  get satisfiedDemand() {
+    return Math.min(this._demand, this.totalProduction);
+  }
+  get DemandCost() {
+    let remainingDemand = this.demand;
+    let result = 0;
+    this.producers
+      .sort((a, b) => a.cost - b.cost)
+      .forEach((p) => {
+        const contribution = Math.min(remainingDemand, p.production);
+        remainingDemand -= contribution;
+        result += contribution * p.cost;
+      });
+    return result;
+  }
+}
+
+class Producer {
+  constructor(aProvince, data) {
+    this._province = aProvince;
+    this._cost = data.cost;
+    this._name = data.name;
+    this._production = data.production || 0;
+  }
+  get name() {
+    return this._name;
+  }
+  get cost() {
+    return this._cost;
+  }
+  set cost(arg) {
+    this._cost = parseInt(arg);
+  }
+  get production() {
+    return this._production;
+  }
+  set production(amountStr) {
+    const amount = parseInt(amountStr);
+    const newProduction = Number.isNaN(amount) ? 0 : amount;
+    this._province.totalProduction += newProduction - this.production;
+    this._production = newProduction;
+  }
+}
+
+function sampleProvinceData() {
+  return {
+    name: 'Asia',
+    producers: [
+      { name: 'Byzantium', cost: 10, production: 9 },
+      { name: 'Attalia', cost: 12, production: 10 },
+      { name: 'Sinope', cost: 10, production: 6 },
+    ],
+    demand: 30,
+    price: 20,
+  };
+}
