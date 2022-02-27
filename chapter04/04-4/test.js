1e3fe064896ede2721b9489b4d03c1e4a2f03569
365kim <365listener@gmail.com>
2022-02-27 13:14:36 +0900

    refactor: 04-4 fixture 중복코드 제거 (표준 fixture 사용)

--- chapter04/04-4/test.js
+++ chapter04/04-4/test.js
@@ -2,14 +2,16 @@ import { Province, sampleProvinceData } from './index.js';
 import assert from 'assert';
 
 describe('province', function () {
-  it('shortfall', function () {
-    const asia = new Province(sampleProvinceData());
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
 
+  it('shortfall', function () {
     assert.equal(asia.shortfall, 5);
   });
-  it('profit', function () {
-    const asia = new Province(sampleProvinceData());
 
+  it('profit', function () {
     assert.equal(asia.profit, 230);
   });
 });
