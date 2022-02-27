9f73ce3354a6c6851479fd573311e022c63dc31a
365kim <365listener@gmail.com>
2022-02-27 16:10:01 +0900

    test: 04-6 no producers 테스트 추가

---
+++ chapter04/04-6/test.js
@@ -0,0 +1,45 @@
+import { Province, sampleProvinceData } from './index.js';
+import assert from 'assert';
+
+describe('province', function () {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
+
+  it('shortfall', function () {
+    assert.equal(asia.shortfall, 5);
+  });
+
+  it('profit', function () {
+    assert.equal(asia.profit, 230);
+  });
+
+  it('change production', function () {
+    asia.producers[0].production = 20;
+
+    assert.equal(asia.shortfall, -6);
+    assert.equal(asia.profit, 292);
+  });
+
+});
+
+describe('no producers', function () {
+  let noProducers;
+  beforeEach(function () {
+    const data = {
+      name: 'No producers',
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+  it('shortfall', function () {
+    assert.equal(noProducers.shortfall, 30);
+  });
+
+  it('profit', function () {
+    assert.equal(noProducers.profit, 0);
+  });
+});
