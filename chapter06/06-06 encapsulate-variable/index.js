c8a84a79699a70b2a6cbf925c34f5059a89b84f9
365kim <365listener@gmail.com>
2022-03-13 15:50:22 +0900

    feat: 06-06 변수 캡슐화하기 예제 추가

---
+++ chapter06/06-06 encapsulate-variable/index.js
@@ -0,0 +1,4 @@
+let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+
+spaceship.owner = defaultOwner;
+defaultOwner = { firstName: '레베카', lastName: '파슨스' };
---
+++ chapter06/06-06 encapsulate-variable/refactoring.js
@@ -0,0 +1,12 @@
+let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+
+spaceship.owner = defaultOwner();
+defaultOwner = setDefaultOwner({ firstName: '레베카', lastName: '파슨스' });
+
+export function defaultOwner() {
+  return Object.assign({}, defaultOwnerData);
+}
+
+export function setDefaultOwner(arg) {
+  defaultOwnerData = arg;
+}
