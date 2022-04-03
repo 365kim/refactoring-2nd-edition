6bf3184fb20ab9c3cd90d6e51bfdc3afe82713ed
365kim <365listener@gmail.com>
2022-04-03 16:29:58 +0900

    feat: 08-4 문장을 호출한 곳으로 옮기기 예제 코드 추가

---
+++ chapter08/08-4 move-statements-to-callers/refactoring.js
@@ -0,0 +1,24 @@
+// renderPerson은 그대로 둔 채 listRecentPhotos가 location을 다르게 렌더해야할 경우
+
+function renderPerson(outStream, person) {
+  outStream.write(`<p>${person.name}</p>\n`);
+  renderPhoto(outStream, person.photo);
+  emitPhotoData(outStream, person.photo);
+  outStream.write(`<p>제목: ${person.photo.location}</p>`);
+}
+
+function listRecentPhotos(outStream, photos) {
+  photos
+    .filter((p) => p.date > recentDateCutoff())
+    .forEach((p) => {
+      outStream.write('<div>\n');
+      emitPhotoData(outStream, p);
+      outStream.write(`<p>제목: ${p.location}</p>`);
+      outStream.write('</div>\n');
+    });
+}
+
+function emitPhotoData(outStream, photo) {
+  outStream.write(`<p>제목: ${p.title}</p>`);
+  outStream.write(`<p>제목: ${photo.date.toString()}</p>`);
+}
