a9ba810a8eb1e3d57251d2e8a6c4ba9eb7bf1636
365kim <365listener@gmail.com>
2022-04-18 22:17:00 +0900

    feat: 10-1 조건문 분해하기 예제 코드 추가

---
+++ chapter10/10-1 decompose-conditional/refactoring.js
@@ -0,0 +1,21 @@
+charge = summer() ? summerCharge() : regularCharge();
+
+// Or
+
+if (summer()) {
+  charge = summerCharge();
+} else {
+  charge = regularCharge();
+}
+
+function summer() {
+  return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
+
+function summerCharge() {
+  return quantity * plan.summerRate;
+}
+
+function regularCharge() {
+  return quantity * plan.regularRate + plan.regularServiceCharge;
+}
