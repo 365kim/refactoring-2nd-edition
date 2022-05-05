4c4dd4fec5449a3f0c25340d33358a2c659015b4
365kim <365listener@gmail.com>
2022-05-05 17:43:52 +0900

    feat: 11-05 매개변수를 질의함수로 바꾸기 예제 코드 추가

---
+++ chpater11/11-05 replace-parameter-with-query/refactoring.js
@@ -0,0 +1,19 @@
+class Order {
+  get finalPrice() {
+    const basePrice = this.quantity * this.itemPrice;
+    return this.discountedPrice(basePrice);
+  }
+
+  get discountLevel() {
+    return this.quantity > 100 ? 2 : 1;
+  }
+
+  discountPrice(basePrice) {
+    switch (this.discountLevel) {
+      case 1:
+        return basePrice * 0.95;
+      case 2:
+        return basePrice * 0.9;
+    }
+  }
+}
